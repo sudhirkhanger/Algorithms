c9535d62f13e4c7d55524dbb513b511b9d1c1023
Sudhir Singh Khanger <sudhir@sudhirkhanger.com>
2026-05-29 08:52:41 +0530

    0001-TWO-SUM Stats: Time: 29 ms (21.28%), Space: 47.8 MB (59.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.kt
+++ 0001-two-sum/0001-two-sum.kt
@@ -1,19 +1,10 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val numsMap = mutableMapOf<Int, Int>()
-
-        nums.forEachIndexed { i, n ->
-            numsMap[n] = i
+        for (i in nums.indices) {
+            for (j in i+1 until nums.size) {
+                if (nums[i] + nums[j] == target) { return intArrayOf(i, j) }
+            }
         }
-
-        nums.forEachIndexed { i1, item1 ->
-	        val item2 = target - item1
-	        if (numsMap.containsKey(item2)) {
-	            val i2 = numsMap[item2]
-	            if (i2 != null && i2 != i1) return intArrayOf(i1, i2)
-	        }
-        }
-    
-    return intArrayOf(0)
+        return intArrayOf()
     }
 }
